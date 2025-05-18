5a34c89bac3bee2bda7d35ddbe4e5d7df9076bc7
inconsiderate <meiskant@gmail.com>
2025-05-18 16:29:38 -0700

    initial bootstrap for discord bot

---
+++ public_bot/index.js
@@ -0,0 +1,3 @@
+// public_bot/index.js
+
+require('../system_bot/bot');
---
+++ system_bot/bot.js
@@ -0,0 +1,18 @@
+require('dotenv').config({ path: __dirname + '/.env' });
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
+
+client.once('ready', () => {
+  console.log(`🤖 Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('🏓 Pong from system_bot!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
