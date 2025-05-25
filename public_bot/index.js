a4810d8f051833eae2225e2cb22201cc3a4d6fd7
inconsiderate <meiskant@gmail.com>
2025-05-24 17:03:55 -0700

    add ssl false

--- public_bot/index.js
+++ public_bot/index.js
@@ -27,8 +27,10 @@ const mysql = require('mysql2/promise');
 const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT || 3306,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: false
 };
 let db;
 
